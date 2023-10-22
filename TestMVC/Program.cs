4979242f7793c994d6a6badfc49d5f37f51b36e0
VM_Lodoz <kiporovv1@gmail.com>
2023-10-22 19:23:12 +0700

    Connected to db

--- TestMVC/Program.cs
+++ TestMVC/Program.cs
@@ -1,15 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using TestMVC.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MyDbContext>(options => options.UseNpgsql(connection));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
